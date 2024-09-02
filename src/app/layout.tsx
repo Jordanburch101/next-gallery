fc2997897f92727fb1136f996e03b50afd109a9a
Jordan <jordanburch101@gmail.com>
2024-09-02 23:44:12 +1200

    add shadcnui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
            */
           routerConfig={extractRouterConfig(ourFileRouter)}
         />
-        <body>
+        <body className="dark">
           <div className="h-screen grid grid-rows-[auto,1fr]">
             <TopNav />
             <main className="overflow-y-scroll">{children}</main>
