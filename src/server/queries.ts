c0bbc766e8f3c7a0cde81774449cdaa52f7e8f46
Jordan <jordanburch101@gmail.com>
2024-09-03 21:48:57 +1200

    added event capture for deleting images

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -4,6 +4,7 @@ import { auth } from "@clerk/nextjs/server";
 import { and, eq } from "drizzle-orm";
 import { images } from "./db/schema";
 import { redirect } from "next/navigation";
+import analyticsServerCLient from "./analytics";
 
 export async function getMyImages() {
 
@@ -40,6 +41,14 @@ export async function deleteImage(id: number) {
   await db
     .delete(images)
     .where(and(eq(images.id, id), eq(images.userId, user.userId)));
+
+    analyticsServerCLient.capture({
+      distinctId: user.userId,
+      event: 'delete image',
+      properties: {
+        imageId: id
+      }
+    });
 
   redirect("/");
 }
