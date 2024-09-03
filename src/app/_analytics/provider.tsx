64e438a2483cd5ecb2d43474b4d3c83fc68142a7
Jordan <jordanburch101@gmail.com>
2024-09-03 16:34:29 +1200

    basic PostHog analytics

--- src/app/_analytics/provider.tsx
+++ src/app/_analytics/provider.tsx
@@ -1,13 +1,39 @@
 'use client'
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
+import { useAuth, useUser } from '@clerk/nextjs'
+import { use, useEffect } from 'react'
+
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    api_host: "/ingest",
+    ui_host: 'https://us.posthog.com'
   })
 }
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
-    return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+    return ( 
+        <PostHogProvider client={posthog}>
+          <PostHogAuthWrapper>{children}</PostHogAuthWrapper>
+        </PostHogProvider>
+    )
+}
+
+function PostHogAuthWrapper({ children }: { children: React.ReactNode }) {
+    const auth = useAuth()
+    const userInfo = useUser();
+
+
+    useEffect(() => {
+        if (userInfo.user) {
+            posthog.identify(userInfo.user.id, {
+              email: userInfo.user.emailAddresses[0]?.emailAddress,
+              name: userInfo.user.firstName,
+            })
+        } else if (!auth.isSignedIn) {
+            posthog.reset()
+        }
+    }, [auth, userInfo])
+
+    return children;
 }
